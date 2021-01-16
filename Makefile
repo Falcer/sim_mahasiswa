8f205e16c210416e0ef29d0051a3dea5f32bdacb
Argana Phangquestian <arganaphang@gmail.com>
2021-01-16 23:13:40 +0700

    Add Role + Role Seeder

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 run: 
 	docker-compose up -d --build site
+migrateseed:
+	docker-compose run --rm artisan migrate:refresh --seed
--- src/app/Http/Controllers/Auth/RegisteredUserController.php
+++ src/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Role;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
@@ -43,6 +44,8 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]));
+
+        $user->roles()->attach(Role::where('name', 'user')->first());
 
         event(new Registered($user));
 
